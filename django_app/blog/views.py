facef875bddd57d08cf63fa39f34a1c9e2b885ff
thenry0401 <thenry0401@gmail.com>
2017-05-31 23:12:10 +0900

    create base templates base.html, post_list.html make lnk to post_detail

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -20,3 +20,9 @@ def post_detail(request, pk):
         'post' : Post.objects.get(pk=pk)
     }
     return render(request, 'blog/post_detail.html', context)
+
+def post_create(request):
+    context = {
+
+    }
+    return render(request, 'blog/post_create.html', context)
