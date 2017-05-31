119139326f828f49e60c71a457d8bae8cbc92d4d
thenry0401 <thenry0401@gmail.com>
2017-06-01 00:06:28 +0900

    post_create POST request make Post

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,28 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.utils import timezone
+
+User = get_user_model
 
 from .models import Post
 
 
 def post_list(request):
     # posts = Post.objects.all()
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now()
+    posts = Post.objects.order_by(
+        '-created_date'
     )
     context = {
-        'title' : 'PostList from post_list view',
-        'posts' : posts,
+        'title': 'PostList from post_list view',
+        'posts': posts,
     }
     return render(request, 'blog/post_list.html', context=context)
 
+
 def post_detail(request, pk):
     context = {
-        'post' : Post.objects.get(pk=pk)
+        'post': Post.objects.get(pk=pk)
     }
     return render(request, 'blog/post_detail.html', context)
 
-def post_create(request):
-    context = {
 
-    }
-    return render(request, 'blog/post_create.html', context)
+def post_create(request):
+    if request.method == 'GET':
+        context = {
+
+        }
+        return render(request, 'blog/post_create.html', context)
+
+    elif request.method == 'POST':
+        data = request.POST
+        title = data['title']
+        text = data['text']
+        user = User.objects.first()
+        post = Post.objects.create(
+            title=title,
+            text=text,
+            author=user,
+        )
+        return redirect('post_detail', pk=post.pk)
